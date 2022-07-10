030d24db0801ea2cef0b0216174a465e83a3f7dc
Schillerdavis <107785879+Schillerdavis@users.noreply.github.com>
2022-07-09 21:36:30 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function padrao(){
+    document.getElementsByTagName("body")[0].className = ""
+}
+function darkblue(){
+    document.getElementsByTagName("body")[0].className = "darkBlue"
+}
+function pink(){
+    document.getElementsByTagName("body")[0].className = "pink"
+}
+function darkpink(){
+    document.getElementsByTagName("body")[0].className = "darkPink"
+}
+function green(){
+    document.getElementsByTagName("body")[0].className = "green"
+}
+function darkgreen(){
+    document.getElementsByTagName("body")[0].className = "darkGreen"
+}
